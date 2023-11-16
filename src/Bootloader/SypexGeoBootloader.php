f310db7d998ab6d0d2744b7ee66c354c2a988739
gam6itko <gam6itko@gmail.com>
2023-11-16 14:01:55 +0300

    GeoInterface singleton

--- src/Bootloader/SypexGeoBootloader.php
+++ src/Bootloader/SypexGeoBootloader.php
@@ -7,15 +7,16 @@ namespace Gam6itko\Spiral\SypexGeo\Bootloader;
 use Gam6itko\Spiral\SypexGeo\Config\SypexGeoConfig;
 use Spiral\Boot\Bootloader\Bootloader;
 use Sypex\Geo;
+use Sypex\GeoAdapter;
+use Sypex\GeoInterface;
 
 final class SypexGeoBootloader extends Bootloader
 {
-    protected const SINGLETONS = [
-        Geo::class => [self::class, 'initGeo'],
-    ];
-
-    public function initGeo(SypexGeoConfig $config): Geo
+    public function defineSingletons(): array
     {
-        return new Geo($config->getFilepath(), $config->getMode());
+        return [
+            Geo::class => static fn(SypexGeoConfig $config) => new Geo($config->getFilepath(), $config->getMode()),
+            GeoInterface::class => static fn(Geo $geo) => new GeoAdapter($geo),
+        ];
     }
 }
